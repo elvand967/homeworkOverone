17e61d92718be46a836c2b53f0c964bd5f2bdb68
elvand967 <110315572+elvand967@users.noreply.github.com>
2022-07-31 10:20:19 +0300

    a list of goods and their cost

--- hwTask06-3.py
+++ hwTask06-3.py
@@ -3,12 +3,23 @@
 # Человек может совершать покупки, пока у него на карте хватает на это денег.
 # В начале программы вводим количество денег, а потом вводим расходы, пока они не превышают баланс на карте.
 # Когда превысили, мы должны получить, сколько успели сделать покупок и сколько осталось денег на карте.
+import random
 
 cashDeposit = 0
+productList = []
+shoppingList = []
 def deposit():
     return print(f'В настоящий момент на депозите {cashDeposit} BYN')
 
 if cashDeposit == 0:
     deposit()
     cashDeposit = float(input('Пополнить депозит на сумму: '))
+print('Welcome to shopping!\n(Добро пожаловать за покупками!)')
 deposit()
+print('Наш ассортимент:')
+for i in  range(0,10):
+    x = random.uniform(5.0,100.99)
+    x = float('{:.2f}'.format(x))
+    productList.insert(i,x)
+    print(f'товар № {i+1}, цена: {productList[i]} BYN')
+
