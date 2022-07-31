387dbe1703d874234b837cb57fe99d4ec653dc3d
elvand967 <110315572+elvand967@users.noreply.github.com>
2022-07-31 12:31:40 +0300

    corrected grammatical error

--- hwTask06-3.py
+++ hwTask06-3.py
@@ -6,10 +6,8 @@
 import random
 cashDeposit = 0
 productList = []
-
 def deposit():
     return print(f'В настоящий момент на депозите {cashDeposit} BYN')
-
 if cashDeposit == 0:
     deposit()
     cashDeposit = float(input('Пополнить депозит на сумму: '))
@@ -21,14 +19,11 @@ for i in  range(0,10):
     x = float('{:.2f}'.format(x))
     productList.insert(i,x)
     print(f'товар № {i+1}, цена: {productList[i]} BYN')
-
 shoppingList = list(map(int, input('\nВведите номера товаров (через пробел), для помещения их в карзину: ').split()))
-# print(shoppingList)
-
 for i in range(len(shoppingList)):
     if  cashDeposit >= productList[shoppingList[i]-1]:
         cashDeposit -= productList[shoppingList[i]-1]
         print(f'Приобретен товар № {shoppingList[i]} по цене:  {productList[shoppingList[i]-1]} BYN')
     else:
-        print(f'Недостаточно средст для приобретения товара № {shoppingList[i]} по цене:  {productList[shoppingList[i]-1]} BYN')
+        print(f'Недостаточно средств для приобретения товара № {shoppingList[i]} по цене:  {productList[shoppingList[i]-1]} BYN')
 print(f"\nОстаток средств на счету {float('{:.2f}'.format(cashDeposit))} BYN")
