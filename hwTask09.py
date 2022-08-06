6286ab0cdc5d375471096735e5b6e51df4421ac4
elvand967 <110315572+elvand967@users.noreply.github.com>
2022-08-06 09:17:01 +0300

    nesting a new list at the position of the first element

--- hwTask09.py
+++ hwTask09.py
@@ -5,3 +5,5 @@ from random import randint
 # создаем список из 7 элементов, с случайными числами от 0 до 100 при помощи генератора списков
 mylist = [randint(0,100) for i in range(7)]
 print(mylist)
+mylist[0] = [1, 2, 3]
+print(mylist)
