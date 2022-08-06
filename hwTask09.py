c98bae2ce19337bc0fdde4ffe5bde16b69458dcb
elvand967 <110315572+elvand967@users.noreply.github.com>
2022-08-06 11:55:17 +0300

    Task #9 report formatted

--- hwTask09.py
+++ hwTask09.py
@@ -2,27 +2,19 @@
 # Создай список, замени первый его элемент на [1, 2, 3],
 # затем в конец списка добавь сумму элементов вложенного списка.
 from random import randint
-#from string import ljust
 # создаем список из 7 элементов, с случайными числами от 0 до 100 при помощи генератора списков
-coment = ['Исходный список:','в 1-ую позицию вложен новый список:','в конец списка добавлена сумму элементов вложенного списка:' ]
+coment = ['Исходный список:',
+          'в 1-ую позицию вложен новый список:',
+          'в конец списка добавлена сумму элементов вложенного списка:' ]
 for i in range(len(coment)):
     if not i:
         n =len(coment[i])
-        print(n)
         continue
     if len(coment[i]) > n:
         n = len(coment[i])
-        print(n)
-for i in range(len(coment)):
-    s = coment[i]
-    s.ljust(60,'*')
-    print(s)
-    coment[i] = s
-    print(coment[i])
-
 mylist = [randint(0,100) for i in range(7)]
-print(f'{coment[0]}{mylist}')
+print(coment[0].ljust(n,' '),mylist)
 mylist[0] = [1, 2, 3]
-print(f'{coment[1]}{mylist}')
+print(coment[1].ljust(n,' '),mylist)
 mylist.append(sum(mylist[0]))
-print(f'{coment[2]}{mylist}')
+print(coment[2].ljust(n,' '),mylist)
