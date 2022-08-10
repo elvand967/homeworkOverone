6283188fceb472b78cd0b7da07f955087df5ace2
elvand967 <110315572+elvand967@users.noreply.github.com>
2022-08-10 19:00:07 +0300

    task No. 12-2, 2nd option for generating a number with a given number of digits and solving the problem

--- hwTask12-2.py
+++ hwTask12-2.py
@@ -6,7 +6,13 @@ import random
 DigitGuantity = 0
 NumberDigits = int(input('Введите количество цифр в исходном числе: '))
 DesiredFigure = int(input('Введите искомую цифру (от 0 до 9): '))
-a = int('1' + ('0'*(NumberDigits-1)))
-b = int('9'*NumberDigits)
-Number = random.randrange(a, b)
-print(Number)
+# a = int('1' + ('0'*(NumberDigits-1)))
+# b = int('9'*NumberDigits)
+# Number = random.randrange(a, b)
+NumberList = [random.randint(0,9) for i in range(NumberDigits)]
+Number = int(''.join(map(str, NumberList)))
+for i in NumberList:
+    if i == DesiredFigure:
+        DigitGuantity += 1
+print(f'В числе {Number} цифра {DesiredFigure} встречается: {DigitGuantity} раз')
+
