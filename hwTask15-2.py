90cafda4da4242b9bd2c1fbfd3c761cc9036a48d
elvand967 <110315572+elvand967@users.noreply.github.com>
2022-08-14 08:37:37 +0300

    solution a Task No. 15-2

--- hwTask15-2.py
+++ hwTask15-2.py
@@ -1,16 +1,23 @@
 # Домашнее задание Задача №15-2
 # Дан словарь с числовыми значениями.
 # Необходимо их все перемножить и вывести на экран
-
 while True:
-    work = 0
+    work = 1
     amount = input('Введите количество элементов словаря с числовыми значениями: ')
     if amount.isdigit():
         amount = int(amount)
-        numericaldict = {a: a ** 2 for a in range(1,amount)}
-        print(f'иследуемый словарь: {numericaldict}')
+        numericaldict = {a: (a+1) ** 2 for a in range(amount)}
+        print(f'Иследуемый словарь: {numericaldict}')
     else:
         print('Некорректный ввод данных')
         if input('Повторить попытку? (Y/N): ').upper() == 'Y':
             continue
         else: break
+    for k, v in numericaldict.items():
+        work *= v
+    print(f'Произведение всех значений словаря: {work}')
+    if input('-------------\nПовторить тест? (Y/N): ').upper() == 'Y':
+        continue
+    else:
+        break
+print('Программа завершила работу')
