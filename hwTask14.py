075acafc141837b5273ce7ee884f8457de1078bb
elvand967 <110315572+elvand967@users.noreply.github.com>
2022-08-14 05:32:36 +0300

    task No. 14, condition and requirements

---
+++ hwTask14.py
@@ -0,0 +1,29 @@
+# Домашнее задание Задача №14
+# Есть словарь песен группы Depeche Mode
+# violator
+# songsdict = {
+# 'World in My Eyes': 4.76,
+# 'Sweetest Perfection': 5.43,
+# 'Personal Jesus': 4.56,
+# 'Halo': 4.30,
+# 'Waiting for the Night': 6.07,
+# 'Enjoy the Silence': 4.6,
+# 'Policy of Truth': 4.88,
+# 'Blue Dress': 4.18,
+# 'Clean': 5.68,
+# }
+# Выведите общее время звучания всех песен.
+# Создайте список песен, время звучаниях которых больше 5 минут
+# Создайте новый словарь тех песен, в название которых состоит из одного слова
+
+songsdict = {
+'World in My Eyes': 4.76,
+'Sweetest Perfection': 5.43,
+'Personal Jesus': 4.56,
+'Halo': 4.30,
+'Waiting for the Night': 6.07,
+'Enjoy the Silence': 4.6,
+'Policy of Truth': 4.88,
+'Blue Dress': 4.18,
+'Clean': 5.68,
+}
