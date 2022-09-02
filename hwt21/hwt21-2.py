a52c049829240306fa0c6ad029b4c4685e3e6052
elvand967 <110315572+elvand967@users.noreply.github.com>
2022-09-02 17:40:38 +0300

    solution a Task No. 21-2

--- hwt21/hwt21-2.py
+++ hwt21/hwt21-2.py
@@ -2,3 +2,16 @@
 # Даны два списка чисел.
 # Посчитайте, сколько чисел содержится одновременно как в первом списке, так и во втором.
 import random
+# функция генерации рондонных списков рондонных чисел
+def myRanList(n1=1,n2=99,l1=5,l2=20):
+    MyRanList = [random.randint(n1,n2) for a in range(random.randint(l1,l2))]
+    return MyRanList
+
+list_a = myRanList(1,15,5,10)
+list_b = myRanList(1,15,5,10)
+print('Даны два списка чисел:')
+print(list_a)
+print(list_b)
+set_c = set(list_a + list_b)
+print(f'Одновременно в данных списках содержиться {len(set_c)} чисел:\n{set_c}')
+
