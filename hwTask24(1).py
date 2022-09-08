c240ab0103c70055c188cc77fa6b343e7326923b
elvand967 <110315572+elvand967@users.noreply.github.com>
2022-09-08 11:41:38 +0300

    solution a Task No. 24(1) made the task more difficult

--- hwTask24(1).py
+++ hwTask24(1).py
@@ -2,14 +2,15 @@
 # Напишите рекурсивную функцию, которая осуществляет суммирование чисел в списке.
 # Список должен быть сгенерирован из 10 чисел, каждое в диапазоне от 1 до 100
 import random
-def func(testlist, n = -99):
-    if n == -99:
+def func(testlist, n = None):
+    if n is None:
         n = len(testlist)
     if n > 0:
         return testlist[n-1] + func(testlist, n-1)
     else: return 0
 
 mylist = []
-for i in range(10):
+x = random.randint(5,20)
+for i in range(x):
     mylist.append(random.randint(1,100))
-print(f'Сумма чисел списка: {mylist} = {func(mylist)}')
+print(f'Сумма чисел списка (из {x} чисел): {mylist} = {func(mylist)}')
