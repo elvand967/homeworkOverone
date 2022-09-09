fea625d4bae8ad67b7866845509ffb09df93bb82
elvand967 <110315572+elvand967@users.noreply.github.com>
2022-09-09 20:53:36 +0300

    Task No. 26-1 in work(1)

--- hwTask26-1.py
+++ hwTask26-1.py
@@ -3,7 +3,7 @@
 # Ввод с клавиатуры: операции + - * / и два числа. Операции являются функциями.
 # Обработать ошибку: “Деление на ноль”
 # Ноль использовать в качестве завершения программы (сделать как отдельную операцию).
-import math
+
 def f_sum(mylist):
     return float(mylist[0]) + float(mylist[1])
 def f_sub(mylist):
@@ -14,17 +14,23 @@ def f_div(mylist):
     if float(mylist[1]) == 0:
         return 'Деление на "0" недопустимо!'
     return float(mylist[0]) / float(mylist[1])
-mo = input('Введите необходимую математическую операцию (число + - * / второе число): ')
-if mo.find('+')>=0:
-    mylist = mo.split('+')
-    print(f'{mo} = {f_sum(mylist)}')
-elif  mo.find('-')>=0:
-    mylist = mo.split('-')
-    print(f'{mo} = {f_sub(mylist)}')
-elif  mo.find('*')>=0:
-    mylist = mo.split('*')
-    print(f'{mo} = {f_mult(mylist)}')
-elif  mo.find('/')>=0:
-    mylist = mo.split('/')
-    print(f'{mo} = {f_div(mylist)}')
+while True:
+    mo = input('Для выхода из программы введите "0"\n'
+               'или введите необходимую математическую операцию (число + - * / второе число): ')
+    if mo == '0':
+        break
+    elif mo.find('+')>=0:
+        mylist = mo.split('+')
+        print(f'{mo} = {f_sum(mylist)}')
+    elif  mo.find('-')>=0:
+        mylist = mo.split('-')
+        print(f'{mo} = {f_sub(mylist)}')
+    elif  mo.find('*')>=0:
+        mylist = mo.split('*')
+        print(f'{mo} = {f_mult(mylist)}')
+    elif  mo.find('/')>=0:
+        mylist = mo.split('/')
+        print(f'{mo} = {f_div(mylist)}')
+    else: print('Некорректный ввод данных')
+print('Программа завершила свою работу')
 
