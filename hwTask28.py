4a3b7d39357378da3e16451485ca4613fe9c342a
elvand967 <110315572+elvand967@users.noreply.github.com>
2022-09-15 08:33:49 +0300

    solution a Task No. 28

--- hwTask28.py
+++ hwTask28.py
@@ -1,12 +1,29 @@
 # Домашнее задание Задача №28
 # Допишите несколько атрибутов в класс с прошлого задания, продемонстрируйте их работу
+from random import choice
 
-class Car:
+class Car():
+    car_count = 0
     def __init__(self):
         print ("легковой автомобиль")
         self.name = "corolla"
-        self.__make = "toyota"
-        self._model = 2020
+        self.make = "toyota"
+        self.model = 2020
+        self.color = choice(['white','black','red'])
+        self.tires = choice(['Windforce Catchfors H/P 205/55 R16 91V',
+                    'Kormoran Road Performance 205/55 R16 94V XL','DX640 225/40 ZR18 92W XL FR']) # Шины для авто
+        Car.car_count += 1
 
-car_a = Car()  # легковой автомобиль
-print(car_a.name)  # corolla
+car_a = Car()
+print(f'№ {car_a.car_count} | {car_a.make} | {car_a.name}  | {car_a.model} г.в.\n'
+      f'цвет: {car_a.color}  | шины: {car_a.tires}\n')
+
+car_b = Car()
+print(f'№ {car_b.car_count} | {car_b.make} | {car_b.name}  | {car_b.model} г.в.\n'
+      f'цвет: {car_b.color}  | шины: {car_b.tires}\n')
+
+car_c = Car()
+print(f'№ {car_c.car_count} | {car_c.make} | {car_c.name}  | {car_c.model} г.в.\n'
+      f'цвет: {car_c.color}  | шины: {car_c.tires}\n')
+
+
