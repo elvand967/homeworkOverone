4fab81707d7b7acbb260a5815847f009adde0991
elvand967 <110315572+elvand967@users.noreply.github.com>
2022-09-24 01:59:17 +0300

    Task No. 34 (billing_statement_for_water_utilities)

--- hwTask34(billing_statement_for_water_utilities).py
+++ hwTask34(billing_statement_for_water_utilities).py
@@ -14,8 +14,10 @@
 #   - Норма водопотребления лиц (subsidies = False), обеспечивающий полное возмещение экономически обоснованных затрат
 #     400 л + 400 л. (+водоотведение).
 import random
+from datetime import datetime
 
 class House():
+    count_apartments = None
     default_street = 'В.Гостинец'
     default_house_number = 124
     default_locality = 'Молодечно'
@@ -54,14 +56,19 @@ class House():
 
 
 class Flat(House):
-    apartment_number = 0
+    apartment_number = -1 # шапка ведомости создает дополнительный экземпляр. корректировка (-1)
     default_count_people = None
+    default_subsidies = None
 
-    def __init__(self, count_people=default_count_people):
+    def __init__(self, count_people=default_count_people, subsidies = default_subsidies):
         super().__init__()
         self.count_people = count_people
         if self.count_people is None:
             self.count_people = random.randint(1, 5)
+        self.subsidies = subsidies
+        if subsidies is None:
+            self.subsidies = random.choice([True, False])
+            #self.subsidies = bool(random.getrandbits(1)) # более быстрый генератор, но воспринимается сложнее
         Flat.apartment_number += 1
 
     def info(self):
@@ -71,15 +78,44 @@ class Flat(House):
         print(f'дом: {self.house_number}')
         print(f'квартира: {self.apartment_number}')
         print(f'проживает (зарегестрированно): {self.count_people} человек(а)')
-        print('---------------------------------')
+        print(f'тариф субсидируемый государством: {self.subsidies}')
+        print('--------------------------------------------')
+
+class Vedomosti(Flat):
 
 
+    def __int__(self):
+        super().__init__()
 
-def show_houses():
-    apartments =  House.get_count_apartments()
-    objs = [Flat for i in range(apartments)]
-    for obj in objs:
-        object = obj()
-        object.info()
+    @staticmethod
+    def get_reporting_period():
+        current_datetime = datetime.now()
 
-show_houses()
+        if current_datetime.month != 1:
+            get_period = str(current_datetime.month - 1) + '  ' + str(current_datetime.year) + 'г.'
+        else:
+            get_period = '12   ' + str(current_datetime.year - 1) + 'г.'
+
+        return get_period
+
+    def info(self):
+        n = 90
+        print('\t\t\tведомость коммунальных платежей за потребленную воду и водоотведение')
+        print(f'населенный пункт: {self.locality}')
+        print(f'улица: {self.street}')
+        print(f'дом: {self.house_number}')
+        print(f'отчетный период: {self.get_reporting_period()}\n')
+        print('=' * n)
+        print(f'  № кв.\t|\tпроживает\t|\tтариф,\t\t\t|\t')
+        print(f'\t\t|\t(чел.)\t\t|\tсубсидируемый\t|\t')
+        print(f'\t\t|\t\t\t\t|\tгосударством\t|\t')
+        print('='* n)
+        self.apartments = House.get_count_apartments()
+        self.objs = [Flat for i in range(self.apartments)]
+        for obj in self.objs:
+            object = obj()
+            print(f'\t{object.apartment_number}\t|\t\t{object.count_people}\t\t|\t{object.subsidies}\t\t\t|\t"test"')
+        print('=' * n)
+
+vedomosti = Vedomosti()
+vedomosti.info()
