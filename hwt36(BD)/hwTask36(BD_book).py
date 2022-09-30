030ea745edc2ae842f6b661fb81960c1a0d9c2ea
elvand967 <110315572+elvand967@users.noreply.github.com>
2022-09-30 06:56:18 +0300

    Task No. 36 (bookstore)

---
+++ hwt36(BD)/hwTask36(BD_book).py
@@ -0,0 +1,12 @@
+# Домашнее задание. Задача №36
+# 1.Сформулируйте SQL запрос для создания таблицы book. Структура таблицы book:
+
+# Поле                    |  Тип, описание
+# book_id                 |  INT PRIMARY KEY AUTO_INCREMENT
+# title                   |  VARCHAR(50)
+# author                  |  VARCHAR(30)
+# price                   |  DECIMAL(8, 2)
+# amount                  |  INT
+
+# 2.Занесите новую строку в таблицуbook
+# 3.Выбрать информацию о всех книгах из таблицы book.
